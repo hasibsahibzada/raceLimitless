a8ce34aec33f88fab12d7a79500257ae68d17212
hasibsahibzada <hasibullah@sahibzada.org>
2018-01-03 22:45:50 +0100

    react-native-router-flux installed

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 import Home from './src/components/view/Home';
 import Profile from './src/components/view/Profile';
 import withSideMenu from './src/components/withSideMenu';
+import { Router, Scene } from 'react-native-router-flux';
 
 const HomeWithSideMenu = withSideMenu(Home);
 const ProfileWithSideMenu = withSideMenu(Profile);
 
-const App = StackNavigator({
-    Profile: { screen: ProfileWithSideMenu },
-    Home: { screen: HomeWithSideMenu }
-});
+const App = () => (
+    <Router>
+        <Scene key="root">
+            <Scene type='replace' key="Home" component={HomeWithSideMenu} title="Home" panHandlers={null}/>
+            <Scene type='replace' key="Profile" component={ProfileWithSideMenu} title="Profile" panHandlers={null}/>
+        </Scene>
+    </Router>
+);
 
 export default App;
--- src/components/view/Home.js
+++ src/components/view/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    my Home
+                    my Homes
                 </Text>
             </View>
         );
